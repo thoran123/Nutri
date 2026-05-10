dd38306a77a3f003dcdb6e86d725438bf6feb4a8
Tiến Nguyễn <tiennguyen@Tiens-Macbook.local>
2026-05-11 00:08:50 +1000

    Fix food data and barcode route contracts

--- routes/fooddata.js
+++ routes/fooddata.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controller/foodDatabaseController');
+const foodDatabaseController = require('../controller/foodDatabaseController');
+const foodDataController = require('../controller/foodDataController');
 const validate = require('../middleware/validate');
 const { foodSearchQuery } = require('../validators/schemas');
 
-router.get('/search', validate(foodSearchQuery, 'query'), controller.searchFood);
+router.get('/search', validate(foodSearchQuery, 'query'), foodDatabaseController.searchFood);
+router.get('/dietaryrequirements', foodDataController.getAllDietaryRequirements);
+router.get('/cuisines', foodDataController.getAllCuisines);
+router.get('/allergies', foodDataController.getAllAllergies);
+router.get('/ingredients', foodDataController.getAllIngredients);
+router.get('/cookingmethods', foodDataController.getAllCookingMethods);
+router.get('/spicelevels', foodDataController.getAllSpiceLevels);
+router.get('/healthconditions', foodDataController.getAllHealthConditions);
 
 module.exports = router;
